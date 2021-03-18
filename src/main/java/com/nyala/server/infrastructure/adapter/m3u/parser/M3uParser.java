47e3043b8595ab204ffa82e08beee3dd5fb54f31
David Fernandez <morenza@gmail.com>
2021-03-18 07:45:26 +0000

    Tidied parser

--- src/main/java/com/nyala/server/infrastructure/adapter/m3u/parser/M3uParser.java
+++ src/main/java/com/nyala/server/infrastructure/adapter/m3u/parser/M3uParser.java
@@ -13,7 +13,6 @@ public class M3uParser {
 
     public static final String TAG_MARKER = "#";
     public static final String EXTM3U = "#EXTM3U";
-    private final M3uMediaTagParser m3uMediaTagParser = new M3uMediaTagParser();
 
     public M3uPlaylist parse(String m3uText) {
         if (!m3uText.startsWith(EXTM3U)) {
@@ -36,22 +35,20 @@ public class M3uParser {
 
             if (isTopHeader(line)) {
                 log.debug("Skipping starting tag");
-                line = m3uLinesIterator.next();
+                if (m3uLinesIterator.hasNext()) {
+                    line = m3uLinesIterator.next();
+                } else {
+                    // returns empty playlist
+                    return playlistBuilder.build();
+                }
             }
 
             if (isMediaTag(line)) {
                 M3uMediaTag m3uMediaTag = m3uMediaTagParser.parseMediaTag(line);
                 if (m3uLinesIterator.hasNext()) {
                     String mediaUri = m3uLinesIterator.next();
-
-                    if (isMediaUri(mediaUri)) {
-                        M3uMediaUri m3uMediaUri = M3uMediaUri.builder()
-                                .uri(mediaUri)
-                                .build();
-                        playlistBuilder.addMediaEntry(m3uMediaTag, m3uMediaUri);
-                    } else {
-                        throw new RuntimeException("Invalid M3U playlist: media tag not followed by media uri");
-                    }
+                    M3uMediaUri m3uMediaUri = parseMediaUriIfPresent(mediaUri);
+                    playlistBuilder.addMediaEntry(m3uMediaTag, m3uMediaUri);
                 } else {
                     throw new RuntimeException("Invalid M3U playlist: media tag not followed by media uri");
                 }
@@ -61,23 +58,14 @@ public class M3uParser {
         return playlistBuilder.build();
     }
 
-
-    //TODO: https://stackoverflow.com/questions/34086461/java-stream-is-there-a-way-to-iterate-taking-two-elements-a-time-instead-of-one
-    public M3uPlaylist parseHeader(M3uPlaylist.Builder m3uPlaylistBuilder, String header) {
-
-        // skip top header
-        if (!isTopHeader(header)) {
-            if (isMediaTag(header)) {
-                M3uMediaTag m3uMediaTag = m3uMediaTagParser.parseMediaTag(header);
-                m3uPlaylistBuilder.addMediaEntry(m3uMediaTag, null);
-            }
+    private M3uMediaUri parseMediaUriIfPresent(String mediaUri) {
+        if (isMediaUri(mediaUri)) {
+            return M3uMediaUri.builder()
+                    .uri(mediaUri)
+                    .build();
+        } else {
+            throw new RuntimeException("Invalid M3U playlist: media tag not followed by media uri");
         }
-
-        return m3uPlaylistBuilder.build();
-    }
-
-    public M3uPlaylist parseLocation(M3uPlaylist.Builder m3uPlaylistBuilder, String location) {
-        return m3uPlaylistBuilder.build();
     }
 
     private boolean isTopHeader(String header) {
@@ -87,11 +75,8 @@ public class M3uParser {
     private boolean isMediaTag(String mediaTagLine) {
         return mediaTagLine.startsWith(M3uMediaTagParser.EXTINF_TAG);
     }
+
     private boolean isMediaUri(String mediaUriLine) {
         return !mediaUriLine.startsWith(TAG_MARKER);
-    }
-
-    private boolean isHeader(String m3uLine) {
-        return m3uLine.startsWith(TAG_MARKER);
     }
 }
--- src/test/java/com/nyala/server/test/unit/M3uParserTest.java
+++ src/test/java/com/nyala/server/test/unit/M3uParserTest.java
@@ -5,6 +5,7 @@ import com.nyala.server.infrastructure.adapter.m3u.M3uMediaUri;
 import com.nyala.server.infrastructure.adapter.m3u.M3uPlaylist;
 import com.nyala.server.infrastructure.adapter.m3u.M3uPlaylistEntry;
 import com.nyala.server.infrastructure.adapter.m3u.MediaSegmentDuration;
+import com.nyala.server.infrastructure.adapter.m3u.TvgData;
 import com.nyala.server.infrastructure.adapter.m3u.parser.M3uParser;
 import io.lindstrom.m3u8.model.MediaPlaylist;
 import io.lindstrom.m3u8.parser.MediaPlaylistParser;
@@ -79,6 +80,58 @@ public class M3uParserTest {
 
         assertThat(m3uPlaylist.isEmpty(), is(false));
         assertEntriesMatch(m3uPlaylist);
+    }
+
+    @Test
+    public void shouldBuildEntryPairMediaTagMediaUri() {
+        // Given
+        M3uPlaylist.Builder builder = M3uPlaylist.builder();
+        String duration = "-1";
+        String tvgId = "";
+        String tvgLogo = "logo";
+        String tvgName = "aName";
+        String groupTitle = "group";
+        String mediaUri = "http://anUrl";
+
+        MediaSegmentDuration mediaSegmentDuration = MediaSegmentDuration.builder()
+                .duration(duration)
+                .build();
+
+        TvgData tvgData = TvgData.builder()
+                .tvgId(tvgId)
+                .tvgLogo(tvgLogo)
+                .tvgName(tvgName)
+                .groupTitle(groupTitle)
+                .build();
+
+        M3uMediaTag m3uMediaTag = M3uMediaTag.builder()
+                .duration(mediaSegmentDuration)
+                .tvgData(tvgData)
+                .build();
+
+        M3uMediaUri m3uMediaUri = M3uMediaUri.builder().uri(mediaUri).build();
+
+        // When
+        builder.withStart()
+                .addMediaEntry(m3uMediaTag, m3uMediaUri);
+
+        M3uPlaylist playlist = builder.build();
+
+        // Then
+        assertThat(playlist.isEmpty(), is(false));
+        assertThat(playlist.entries().size(), is(1));
+
+        // And
+        M3uPlaylistEntry m3uPlaylistEntry = playlist.entries().get(0);
+        M3uMediaTag tag = m3uPlaylistEntry.mediaTag();
+        M3uMediaUri entryMediaUri = m3uPlaylistEntry.mediaUri();
+
+        assertThat(tag.duration().asIntegerSeconds(), is(Integer.parseInt(duration)));
+        assertThat(entryMediaUri.getUri(), is(mediaUri));
+        assertThat(tag.tvgData().groupTitle(), is(groupTitle));
+        assertThat(tag.tvgData().tvgId(), is(tvgId));
+        assertThat(tag.tvgData().tvgLogo(), is(tvgLogo));
+        assertThat(tag.tvgData().tvgName(), is(tvgName));
     }
 
     private void assertEntriesMatch(M3uPlaylist m3uPlaylist) {
